ad7018bc0f037316e424c306492ad21ddf801429
p3t3r276 <p3t3r276@gmail.com>
2020-06-19 15:04:39 +0700

    sign user out

--- scripts/auth.js
+++ scripts/auth.js
@@ -13,3 +13,13 @@ signupForm.addEventListener("submit", e => {
     signupForm.reset();
   });
 });
+
+// logout
+const logout = document.querySelector("#logout");
+logout.addEventListener("click", e => {
+  e.preventDefault();
+
+  auth.signOut().then(() => {
+    console.log("user sign out");
+  });
+});
