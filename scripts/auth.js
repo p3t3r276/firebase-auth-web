328e8452ef6c93386c98ffca0b73c8fed2ed2b2e
p3t3r276 <p3t3r276@gmail.com>
2020-06-19 15:26:10 +0700

    login

--- scripts/auth.js
+++ scripts/auth.js
@@ -23,3 +23,20 @@ logout.addEventListener("click", e => {
     console.log("user sign out");
   });
 });
+
+//login
+const loginForm = document.querySelector("#login-form");
+loginForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  // user info
+  const email = loginForm["login-email"].value;
+  const password = loginForm["login-password"].value;
+
+  auth.signInWithEmailAndPassword(email, password).then(cred => {
+    console.log(cred.user);
+    const modal = document.querySelector("#modal-login");
+    M.Modal.getInstance(modal).close();
+    loginForm.reset();
+  });
+});
