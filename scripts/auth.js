7421f073149a7b5f1046ea5f4dee0824e3d108eb
p3t3r276 <p3t3r276@gmail.com>
2020-06-23 17:19:19 +0700

    adding guides

--- scripts/auth.js
+++ scripts/auth.js
@@ -10,6 +10,26 @@ auth.onAuthStateChanged(user => {
   }
 });
 
+// create new guide
+const createForm = document.querySelector("#create-form");
+createForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  db.collection("guides")
+    .add({
+      title: createForm["title"].value,
+      content: createForm["content"].value
+    })
+    .then(() => {
+      const modal = document.querySelector("#modal-create");
+      M.Modal.getInstance(modal).close();
+      createForm.reset();
+    })
+    .catch(err => {
+      console.log(err);
+    });
+});
+
 const signupForm = document.querySelector("#signup-form");
 signupForm.addEventListener("submit", e => {
   e.preventDefault();
