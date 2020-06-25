d194e2229c9315f00f113f52ff60509c9f5d73af
Peter Khoa Lê Minh <p3t3r276@gmail.com>
2020-06-25 17:57:18 +0700

    could fn for adding claims

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,21 @@
-const functions = require('firebase-functions');
+const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+exports.addAdminRole = functions.https.onCall((data, context) => {
+  // get user and custom claim (admin)
+  return admin
+    .auth()
+    .getUser(data.email)
+    .then((user) => {
+      return admin.auth().setCustomUserClaims(user.uid, {
+        admin: true,
+      });
+    })
+    .then(() => {
+      return {
+        message: `Success! ${data.email} has been made an admin`,
+      };
+    })
+    .catch((err) => err);
+});
