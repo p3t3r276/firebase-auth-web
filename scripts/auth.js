7eff9a7cc0e5a10e1a8d7e7aa3b76760238173b1
p3t3r276 <p3t3r276@gmail.com>
2020-06-26 19:56:19 +0700

    implement add claims on client

--- scripts/auth.js
+++ scripts/auth.js
@@ -1,3 +1,15 @@
+// add admin cloud function
+const adminForm = document.querySelector(".admin-actions");
+adminForm.addEventListener("submit", e => {
+  e.preventDefault();
+
+  const adminEmail = document.querySelector("#admin-email").value;
+  const adminRole = functions.httpsCallable("addAdminRole");
+  adminRole({ email: adminEmail }).then(result => {
+    console.log(result);
+  });
+});
+
 auth.onAuthStateChanged(user => {
   if (user) {
     db.collection("guides").onSnapshot(
